3d5d2bf697a1681e3a8c1d47b50ac1c9c73b7f39
Mariia Zaripova <zaripova.mary.m@gmail.com>
2023-10-05 16:32:32 +0300

    HW databases-2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,67 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        firts_form = self.forms[0].cleaned_data
+        article = firts_form.get('article')
+        if not article:
+            raise ValidationError('Непредвиденная ошибка')
+
+        old_tags = article.tags.all()
+        tags = {}
+        main_tag = None
+
+        for tag in old_tags:
+            old_tag = Scope.objects.filter(tag=tag).first()
+            if old_tag.is_main:
+                main_tag = old_tag
+
+        for form in self.forms:
+            res = form.cleaned_data
+            new_tag = res.get('tag', '')
+            is_main = res.get('is_main')
+
+            if not isinstance(new_tag, Tag):
+                raise ValidationError('Разделы не должны быть пустыми')
+
+            if new_tag in tags or new_tag in old_tags:
+                raise ValidationError('Каждый раздел может быть добавлен только 1 раз')
+
+            if is_main:
+                if not main_tag:
+                    main_tag = new_tag
+                else:
+                    raise ValidationError('Основным может быть только 1 раздел')
+
+            if new_tag not in old_tags:
+                tags[new_tag] = is_main
+
+        if not main_tag:
+            raise ValidationError('Укажите основной раздел')
+
+        for tag, is_main in tags.items():
+            Scope.objects.create(article=article, tag=tag, is_main=is_main)
+
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    fields = []
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -4,13 +4,20 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    tags = models.ManyToManyField(Tag, related_name='articles')
+    tags = models.ManyToManyField(Tag, related_name='articles', through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -22,9 +29,15 @@ class Article(models.Model):
 
 
 class Scope(models.Model):
-    atricle = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
 
-    def is_main(self):
-        pass
+    class Meta:
+        verbose_name_plural = 'Тематики статьи'
+        verbose_name = 'Тематика статьи'
+        # ordering = ['tag.name']
+
+    # def is_main(self):
+    #     pass
 
