b05b37f535a6f31bfaccd99062b2ae7841c3b2e6
Mariia Zaripova <zaripova.mary.m@gmail.com>
2023-10-19 14:33:43 +0400

    some changes

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -7,17 +7,47 @@ from .models import Article, Scope, Tag
 
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
-        is_main = False
+        firts_form = self.forms[0].cleaned_data
+        article = firts_form.get('article')
+        if not article:
+            raise ValidationError('Непредвиденная ошибка')
+
+        old_tags = article.tags.all()
+        tags = {}
+        main_tag = None
+
+        for tag in old_tags:
+            old_tag = Scope.objects.filter(tag=tag).first()
+            if old_tag.is_main:
+                main_tag = old_tag
+
         for form in self.forms:
-            if form.cleaned_data.get('is_main'):
-                is_main = True
+            res = form.cleaned_data
+            new_tag = res.get('tag', '')
+            is_main = res.get('is_main')
 
-        if is_main:
-            raise ValidationError('Должен быть указан хотя бы 1 главный раздел')
-        else:
-            raise ValidationError('Не может быть больше одного главного раздела')
+            if not isinstance(new_tag, Tag):
+                raise ValidationError('Разделы не должны быть пустыми')
 
-        return super().clean()  # вызываем базовый код переопределяемого метода
+            if new_tag in tags or new_tag in old_tags:
+                raise ValidationError('Каждый раздел может быть добавлен только 1 раз')
+
+            if is_main:
+                if not main_tag:
+                    main_tag = new_tag
+                else:
+                    raise ValidationError('Основным может быть только 1 раздел')
+
+            if new_tag not in old_tags:
+                tags[new_tag] = is_main
+
+        if not main_tag:
+            raise ValidationError('Укажите основной раздел')
+
+        for tag, is_main in tags.items():
+            Scope.objects.create(article=article, tag=tag, is_main=is_main)
+
+        return super().clean()   # вызываем базовый код переопределяемого метода
 
 
 class ScopeInline(admin.TabularInline):
